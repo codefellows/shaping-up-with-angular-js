bd89f1fd37f4f2899bcc748dc17467d6bd4579c0
Brook Riggio <brooke.riggio@gmail.com>
2014-10-10 01:45:18 -0700

    Working with Tabs:
    Adds some new directives, and keeps code clean in new controllers.

--- js/app.js
+++ js/app.js
@@ -5,18 +5,40 @@
     this.product = gems;
   });
 
+  app.controller('PanelController', function() {
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
   var gems = [
     {
       name: 'Dodecahedron',
       price: 2,
       description: 'Look at all those sides!',
-      canPurchase: true
+      canPurchase: true,
+      images: [
+        "http://cl.zdn.io/UuY7/gem-02.gif",
+        "http://cl.zdn.io/UuWP/gem-05.gif",
+        "http://cl.zdn.io/UuD4/gem-09.gif"
+      ],
     },
     {
       name: 'Pentahedron',
       price: 2.95,
       description: 'Looky there!',
-      canPurchase: true
+      canPurchase: true,
+      images: [
+        "http://cl.zdn.io/UvAK/gem-01.gif",
+        "http://cl.zdn.io/Uuwd/gem-03.gif",
+        "http://cl.zdn.io/UuVP/gem-04.gif",
+      ],
     },
   ];
 })();
