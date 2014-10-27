ae2395b56daf2b491dfcc9266c0d85a875c3b162
Brook Riggio <brooke.riggio@gmail.com>
2014-10-27 01:05:00 -0700

    Integrate the http service:
    Break out data to products.json. Pull in this data with Angular Services.

--- js/app.js
+++ js/app.js
@@ -1,79 +1,12 @@
 (function() {
-  var app = angular.module('store', [ ]);
+  var app = angular.module('store', ['store-products']);
 
-  app.controller('StoreController', function() {
-    this.product = gems;
-  });
+  app.controller('StoreController', [ '$http', function($http) {
+    var store = this;
+    store.products = [];
 
-  app.controller('ReviewController', function() {
-    this.review = {};
-
-    this.addReview = function(product) {
-      product.reviews.push(this.review);
-      this.review = {};
-    }
-  });
-
-  app.directive('productTitle', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'product-title.html'
-    }
-  });
-
-  app.directive('productPanels', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'product-panels.html',
-      controller: function() {
-        this.tab = 1;
-
-        this.selectTab = function(setTab) {
-          this.tab = setTab;
-        };
-
-        this.isSelected = function(checkTab) {
-          return this.tab === checkTab;
-        };
-      },
-      controllerAs: 'panel'
-    }
-  });
-
-  var gems = [
-    {
-      name: 'Dodecahedron',
-      price: 2,
-      description: 'Look at all those sides!',
-      canPurchase: true,
-      images: [
-        "http://cl.zdn.io/UuY7/gem-02.gif",
-        "http://cl.zdn.io/UuWP/gem-05.gif",
-        "http://cl.zdn.io/UuD4/gem-09.gif"
-      ],
-      reviews: [
-        {
-          stars: 5,
-          body: "This one is really neat!",
-          author: "joe@mama.com"
-        },
-        {
-          stars: 3,
-          body: "I've had better",
-          author: "meh@sok.com"
-        }
-      ]
-    },
-    {
-      name: 'Pentahedron',
-      price: 2.95,
-      description: 'Looky there!',
-      canPurchase: true,
-      images: [
-        "http://cl.zdn.io/UvAK/gem-01.gif",
-        "http://cl.zdn.io/Uuwd/gem-03.gif",
-        "http://cl.zdn.io/UuVP/gem-04.gif",
-      ],
-    },
-  ];
+    $http.get('/products.json').success(function(data) {
+      store.products = data;
+    })
+  }]);
 })();
---
+++ js/products.js
@@ -0,0 +1,37 @@
+(function(){
+  var app = angular.module('store-products', [ ]);
+    app.controller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+      this.review = {};
+    }
+  });
+
+  app.directive('productTitle', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-title.html'
+    }
+  });
+
+  app.directive('productPanels', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-panels.html',
+      controller: function() {
+        this.tab = 1;
+
+        this.selectTab = function(setTab) {
+          this.tab = setTab;
+        };
+
+        this.isSelected = function(checkTab) {
+          return this.tab === checkTab;
+        };
+      },
+      controllerAs: 'panel'
+    }
+  });
+})();
